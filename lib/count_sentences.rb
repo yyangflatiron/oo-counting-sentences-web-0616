a3555ab38847bc78c8deed6151b13be10481b134
Yichen Yang <yichen.yang@flatironschool.com>
2016-06-10 22:07:20 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    counter = 0
+    self.split.each do |word|
+      if
+        word.end_with?(".", "!", "?")
+        counter += 1
+      end
+    end
+    counter
   end
+
+
 end
